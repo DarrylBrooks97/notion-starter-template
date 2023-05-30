61f9528e653b16f58827c0f41944885dfdd873fa
Darryl Brooks <darrylbrooks13@gmail.com>
2023-05-30 10:05:59 -0500

    feat: adding sonner toasts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./styles/globals.css";
+import BaseToast from "./components/BaseToast";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -8,10 +9,13 @@ export const metadata = {
   description: "A starter template for Notion",
 };
 
+export const dynamic = "force-dynamic";
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <BaseToast />
         <main className="flex h-screen w-screen items-center justify-center bg-black">
           {children}
         </main>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,11 @@
-import { CONSTANTS } from "./constants";
+import ConnectButton from "./components/ConnectButton";
 
 export default function Home() {
   return (
     <div className="text-white">
       <div className="flex flex-col items-center space-y-2">
         <h1 className="text-4xl">Notion Starter Template</h1>
-        <a
-          href={CONSTANTS.NOTION_REDIRECT_URI}
-          className="w-max rounded-md border border-slate-500 px-3 py-1 text-xl"
-        >
-          Connect to Notion
-        </a>
+        <ConnectButton />
       </div>
     </div>
   );
